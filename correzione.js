1284934673e341dbc3f0fd856bcf2030330836b7
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-10 10:18:21 +0100

    post correzione

--- correzione.js
+++ correzione.js
@@ -1,9 +1,9 @@
 // inizio controllo email
 const verifyEmail = document.querySelector ('#email .row button');
+const email = ['email1@gmaill.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com'];
 
 verifyEmail.addEventListener('click', function () {
 
-    const email = ['email1@gmaill.com', 'email2@gmail.com', 'email3@gmail.com', 'email4@gmail.com', 'email5@gmail.com'];
     const inputEmail = document.querySelector ('#email .row input').value;
     let controllo = false;
     // console.log(inputEmail);
@@ -11,16 +11,16 @@ verifyEmail.addEventListener('click', function () {
 
     for (let i = 0; i < email.length; i++) {
     
-        if(inputEmail == email[i]) {
+        if(inputEmail === email[i]) {
             controllo = true;
-            console.log('utente registrato');
+            // console.log('utente registrato');
             document.querySelector('.risultatoRegistrazione').innerHTML = '<p class="bg-success rounded-pill text-center"> Utente registrato </p>';
             break;
         } 
     }
 
     if(controllo == false){
-        console.log('utente non registrato');
+        // console.log('utente non registrato');
         document.querySelector('.risultatoRegistrazione').innerHTML = '<p class="bg-danger rounded-pill text-center"> Utente non registrato </p>';
     } 
 
@@ -37,26 +37,26 @@ dadiGenerator.addEventListener('click', function () {
     // max = 6;
     // min = 1;
     
-    let numberUser = Math.floor(Math.random() * (6 - 1) + 1);
-    console.log('il tuo numero è ' + numberUser)
+    let numberUser = Math.floor(Math.random() * (6) + 1);
+    // console.log('il tuo numero è ' + numberUser)
     document.querySelector('.utente').innerText = 'Il tuo numero è ' + numberUser;
     
-    let numberBot = Math.floor(Math.random() * (6 - 1) + 1);
-    console.log('il numero del computer è ' + numberBot)  
+    let numberBot = Math.floor(Math.random() * (6) + 1);
+    // console.log('il numero del computer è ' + numberBot)  
     document.querySelector('.computer').innerText = 'Il numero del computer è ' + numberBot;
    
     if (numberBot > numberUser){
-    console.log('Ha vinto il computer');
+    // console.log('Ha vinto il computer');
     document.querySelector('.risultato').innerHTML = '<p class="bg-danger">Hai perso :-(</p>';
     }
     
     if(numberBot < numberUser){
-    console.log('Hai vinto!');
+    // console.log('Hai vinto!');
     document.querySelector('.risultato').innerHTML = '<p class="bg-success rounded">Hai vinto! :-)</p>';
     } 
 
     if(numberBot === numberUser){
-    console.log('Pareggio!!');
+    // console.log('Pareggio!!');
     document.querySelector('.risultato').innerHTML = '<p class="bg-primary">Pareggio! Ritenta!</p>';
     }
 })
